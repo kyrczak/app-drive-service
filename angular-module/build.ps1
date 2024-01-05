cc9a6bf78e7aea19865348d61f7bb6d3ef5c8b03
kyrczak <patkor9@gmail.com>
2024-01-05 20:20:49 +0100

    Refactor build.ps1 scripts to remove duplicate code

--- angular-module/build.ps1
+++ angular-module/build.ps1
@@ -1,13 +1,21 @@
 function main() {
     npm install
     npm run build
-    $title = (Get-Content Dockerfile | Select-String "org.opencontainers.image.title").ToString().Split('=')[1].Trim(); Write-Host $title
-    $version = (Get-Content Dockerfile | Select-String "org.opencontainers.image.version").ToString().Split('=')[1].Trim(); Write-Host $version
+    $title = (Get-Content Dockerfile | Select-String "org.opencontainers.image.title").ToString().Split('=')[1].Trim()
+    Write-Host $title
+
+    $version = (Get-Content Dockerfile | Select-String "org.opencontainers.image.version").ToString().Split('=')[1].Trim()
+    Write-Host $version
+
+    $imageTag = "${title}:${version}"
+    $imageTag = $imageTag -replace '"', ''  # Remove double quotes from the tag
+    Write-Host "Image Tag: $imageTag"
+
     docker build `
         --label "org.opencontainers.image.created=$(Get-Date -Format o)" `
         --label "org.opencontainers.image.title=$title" `
         --label "org.opencontainers.image.version=$version" `
-        -t "${title}:${version}" .
+        -t $imageTag .
 }
 
-main 
+main
